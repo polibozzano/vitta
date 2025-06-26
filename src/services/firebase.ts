d47d57badb943d1bf9169e7b841d9d2879c86caf
Poliana Bozzano <polibozzano@gmail.com>
2025-06-26 09:02:20 -0300

    Projeto Vitta - Painel de Agendamento

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,20 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAMXquLxKxBCII6HMwME6_iMTQikpQSAkI",
-  authDomain: "painelagendamento.firebaseapp.com",
-  projectId: "painelagendamento",
-  storageBucket: "painelagendamento.firebasestorage.app",
-  messagingSenderId: "401720018483",
-  appId: "1:401720018483:web:85824c441a2652de3e4546",
-  measurementId: "G-330WJ9XVRD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
